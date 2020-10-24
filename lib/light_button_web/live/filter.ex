c1601d3a0397ad7d8f4c646bdc2eafe6822be557
jswob <jacswob@gmail.com>
2020-10-24 12:19:04 +0200

    Filtering + Exercise 1

---
+++ lib/light_button_web/live/filter.ex
@@ -0,0 +1,91 @@
+defmodule LightButtonWeb.FilterLive do
+  use LightButtonWeb, :live_view
+
+  alias LightButton.Boats
+
+  def mount(_params, _session, socket) do
+    socket = assign_defaults(socket)
+
+    {:ok, socket, temporary_assigns: [boats: []]}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Daily Boat Rentals</h1>
+    <div id="filter">
+      <div class="filter">
+        <form phx-change="filter">
+          <div class="filters">
+            <select name="type">
+              <%= options_for_select(type_options(), @type) %>
+            </select>
+            <div class="prices">
+              <input type="hidden" name="prices[]" value="" />
+              <%= for price <- ["$", "$$", "$$$"] do %>
+                <%= price_checkbox(%{price: price, checked: price in @prices}) %>
+              <% end %>
+            </div>
+            <a href="#" phx-click="clear-prices">Clear All</a>
+          </div>
+        </form>
+      </div>
+
+      <div class="boats">
+        <%= for boat <- @boats do %>
+          <div class="card">
+            <img src="<%= boat.image %>">
+            <div class="content">
+              <div class="model">
+                <%= boat.model %>
+              </div>
+              <div class="details">
+                <span class="price">
+                  <%= boat.price %>
+                </span>
+                <span class="type">
+                  <%= boat.type %>
+                </span>
+              </div>
+            </div>
+          </div>
+        <% end %>
+      </div>
+    </div>
+    """
+  end
+
+  def handle_event("clear-prices", _, socket) do
+    socket = assign_defaults(socket)
+    {:noreply, socket}
+  end
+
+  def handle_event("filter", %{"type" => type, "prices" => prices}, socket) do
+    params = [type: type, prices: prices]
+
+    boats = Boats.list_boats(params)
+    socket = assign(socket, params ++ [boats: boats])
+    {:noreply, socket}
+  end
+
+  defp type_options do
+    [
+      "All types": "",
+      Fishing: "fishing",
+      Sporting: "sporting",
+      Sailing: "sailing"
+    ]
+  end
+
+  defp assign_defaults(socket) do
+    assign(socket, boats: Boats.list_boats(), type: "", prices: [""])
+  end
+
+  defp price_checkbox(assigns) do
+    ~L"""
+    <input type="checkbox" id="<%= @price %>"
+            name="prices[]" value="<%= @price %>"
+            <%= if @checked, do: "checked" %> />
+    <label for="<%= @price %>"><%= @price %></label>
+    """
+  end
+end
--- lib/light_button_web/router.ex
+++ lib/light_button_web/router.ex
@@ -24,6 +24,7 @@ defmodule LightButtonWeb.Router do
     live "/search", SearchLive
     live "/flights", FlightsLive
     live "/autocomplete", AutocompleteLive
+    live "/filter", FilterLive
   end
 
   # Other scopes may use custom stacks.
