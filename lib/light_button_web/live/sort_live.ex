e4bf7fff550f4be3ddf0c5074d9637e8433f2277
jswob <jacswob@gmail.com>
2020-10-27 14:12:25 +0100

    Sorting - main complete

---
+++ lib/light_button_web/live/sort_live.ex
@@ -0,0 +1,125 @@
+defmodule LightButtonWeb.SortLive do
+  use LightButtonWeb, :live_view
+
+  alias LightButton.Donations
+
+  def mount(_params, _session, socket) do
+    {:ok, socket, temporary_assigns: [donations: []]}
+  end
+
+  def handle_params(params, _url, socket) do
+    page = String.to_integer(params["page"] || "1")
+    per_page = String.to_integer(params["per_page"] || "20")
+
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    paginate_options = %{page: page, per_page: per_page}
+    sort_options = %{sort_by: sort_by, sort_order: sort_order}
+
+    donations =
+      Donations.list_donations(
+        paginate: paginate_options,
+        sort: sort_options
+      )
+
+    socket =
+      assign(socket,
+        options: Map.merge(paginate_options, sort_options),
+        donations: donations
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_event("select-per-page", %{"per-page" => per_page}, socket) do
+    per_page = String.to_integer(per_page)
+
+    paginate_options = %{page: socket.assigns.options.page, per_page: per_page}
+
+    sort_options = %{
+      sort_by: socket.assigns.options.sort_by,
+      sort_order: socket.assigns.options.sort_order
+    }
+
+    donations =
+      Donations.list_donations(
+        paginate: paginate_options,
+        sort: sort_options
+      )
+
+    socket =
+      assign(socket,
+        options: Map.merge(paginate_options, sort_options),
+        donations: donations
+      )
+
+    socket =
+      push_patch(socket,
+        to:
+          Routes.live_path(
+            socket,
+            __MODULE__,
+            page: socket.assigns.options.page,
+            per_page: per_page,
+            sort_by: socket.assigns.options.sort_by,
+            sort_order: socket.assigns.options.sort_order
+          )
+      )
+
+    {:noreply, socket}
+  end
+
+  defp expires_class(donation) do
+    if Donations.almost_expired?(donation), do: "eat-now", else: "fresh"
+  end
+
+  defp pagination_link(socket, text, page, options, class) do
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          page: page,
+          per_page: options.per_page,
+          sort_by: options.sort_by,
+          sort_order: options.sort_order
+        ),
+      class: class
+    )
+  end
+
+  defp sort_link(socket, text, sort_by, options) do
+    text =
+      if sort_by == options.sort_by do
+        text <> emoji(options.sort_order)
+      else
+        text
+      end
+
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          sort_by: sort_by,
+          sort_order: toggle_sort_order(options.sort_order),
+          page: options.page,
+          per_page: options.per_page
+        )
+    )
+  end
+
+  defp toggle_sort_order(sort_order) do
+    case sort_order do
+      :asc ->
+        :desc
+
+      :desc ->
+        :asc
+    end
+  end
+
+  defp emoji(:asc), do: "👇"
+  defp emoji(:desc), do: "☝"
+end
