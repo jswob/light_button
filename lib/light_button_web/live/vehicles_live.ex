c6f1f52f1f408161fd4d55bb68385e6197cd697c
jswob <jacswob@gmail.com>
2020-10-27 13:04:28 +0100

    Pagination - Exercise 1 complete

--- lib/light_button_web/live/vehicles_live.ex
+++ lib/light_button_web/live/vehicles_live.ex
@@ -22,4 +22,43 @@ defmodule LightButtonWeb.VehiclesLive do
 
     {:noreply, socket}
   end
+
+  def handle_event("switch_per_page", %{"per-page" => per_page}, socket) do
+    per_page = String.to_integer(per_page)
+
+    pagination_options = %{page: socket.assigns.options.page, per_page: per_page}
+    vehicles = Vehicles.list_vehicles(paginate: pagination_options)
+
+    socket =
+      assign(socket,
+        vehicles: vehicles,
+        options: pagination_options
+      )
+
+    socket =
+      push_patch(socket,
+        to:
+          Routes.live_path(
+            socket,
+            __MODULE__,
+            page: pagination_options[:page],
+            per_page: pagination_options[:per_page]
+          )
+      )
+
+    {:noreply, socket}
+  end
+
+  defp pagination_view(socket, text, page, per_page, class) do
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          page: page,
+          per_page: per_page
+        ),
+      class: class
+    )
+  end
 end
