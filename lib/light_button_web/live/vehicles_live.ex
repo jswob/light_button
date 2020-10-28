108c6937e1cdf8957a9115bc09c5ee4150e1b584
jswob <jacswob@gmail.com>
2020-10-28 21:13:37 +0100

    Sorting - Exercises 1

--- lib/light_button_web/live/vehicles_live.ex
+++ lib/light_button_web/live/vehicles_live.ex
@@ -11,13 +11,18 @@ defmodule LightButtonWeb.VehiclesLive do
     page = String.to_integer(params["page"] || "1")
     per_page = String.to_integer(params["per_page"] || "5")
 
+    sort_by = String.to_atom(params["sort_by"] || "id")
+    sort_order = String.to_atom(params["sort_order"] || "asc")
+
     pagination_options = %{page: page, per_page: per_page}
-    vehicles = Vehicles.list_vehicles(paginate: pagination_options)
+    sorting_options = %{sort_by: sort_by, sort_order: sort_order}
+
+    vehicles = Vehicles.list_vehicles(paginate: pagination_options, sort: sorting_options)
 
     socket =
       assign(socket,
         vehicles: vehicles,
-        options: pagination_options
+        options: Map.merge(pagination_options, sorting_options)
       )
 
     {:noreply, socket}
@@ -26,13 +31,16 @@ defmodule LightButtonWeb.VehiclesLive do
   def handle_event("switch_per_page", %{"per-page" => per_page}, socket) do
     per_page = String.to_integer(per_page)
 
-    pagination_options = %{page: socket.assigns.options.page, per_page: per_page}
-    vehicles = Vehicles.list_vehicles(paginate: pagination_options)
+    options = socket.assigns.options
+
+    pagination_options = %{page: options.page, per_page: per_page}
+    sorting_options = %{sort_by: options.sort_by, sort_order: options.sort_order}
+    vehicles = Vehicles.list_vehicles(paginate: pagination_options, sort: sorting_options)
 
     socket =
       assign(socket,
         vehicles: vehicles,
-        options: pagination_options
+        options: Map.merge(pagination_options, sorting_options)
       )
 
     socket =
@@ -42,23 +50,61 @@ defmodule LightButtonWeb.VehiclesLive do
             socket,
             __MODULE__,
             page: pagination_options[:page],
-            per_page: pagination_options[:per_page]
+            per_page: pagination_options[:per_page],
+            sort_by: sorting_options[:sort_by],
+            sort_order: sorting_options[:sort_order]
           )
       )
 
     {:noreply, socket}
   end
 
-  defp pagination_view(socket, text, page, per_page, class) do
+  defp pagination_view(socket, text, page, options, class) do
     live_patch(text,
       to:
         Routes.live_path(
           socket,
           __MODULE__,
           page: page,
-          per_page: per_page
+          per_page: options.per_page,
+          sort_by: options.sort_by,
+          sort_order: options.sort_order
         ),
       class: class
     )
   end
+
+  defp sorting_view(socket, text, sort_by, options) do
+    text =
+      if sort_by == options.sort_by do
+        text <> emoji(options.sort_order)
+      else
+        text
+      end
+
+    sort_order =
+      if sort_by == options.sort_by do
+        toggle_sort_order(options.sort_order)
+      else
+        :asc
+      end
+
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          page: options.page,
+          per_page: options.per_page,
+          sort_by: sort_by,
+          sort_order: sort_order
+        )
+    )
+  end
+
+  defp toggle_sort_order(:asc), do: :desc
+  defp toggle_sort_order(:desc), do: :asc
+
+  defp emoji(:asc), do: "👇"
+  defp emoji(:desc), do: "☝"
 end
