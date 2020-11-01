a7dcf7bc62176dbc2190289278aba613bd998ef7
jswob <jacswob@gmail.com>
2020-11-01 13:55:23 +0100

    Form create - finish

--- lib/light_button_web/live/volunteers_live.ex
+++ lib/light_button_web/live/volunteers_live.ex
@@ -2,15 +2,43 @@ defmodule LightButtonWeb.VolunteersLive do
   use LightButtonWeb, :live_view
 
   alias LightButton.Volunteers
+  alias LightButton.Volunteers.Volunteer
 
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
 
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+
     socket =
       assign(socket,
-        volunteers: volunteers
+        volunteers: volunteers,
+        changeset: changeset
       )
 
-    {:ok, socket}
+    {:ok, socket, temporary_assigns: [volunteers: []]}
+  end
+
+  def handle_event("save", %{"volunteer" => params}, socket) do
+    case Volunteers.create_volunteer(params) do
+      {:ok, volunteer} ->
+        socket =
+          update(
+            socket,
+            :volunteers,
+            fn volunteers -> [volunteer | volunteers] end
+          )
+
+        changeset = Volunteers.change_volunteer(%Volunteer{})
+
+        socket = assign(socket, changeset: changeset)
+
+        :timer.sleep(500)
+
+        {:noreply, socket}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        socket = assign(socket, changeset: changeset)
+        {:noreply, socket}
+    end
   end
 end
